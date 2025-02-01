4f0a2d7e6b935af1ff10cdc655414f68963f866b
Mihai-Cristian Olteanu <mihaicristian.olteanu98@gmail.com>
2025-02-01 20:46:52 +0000

    Section 1.1 done

--- code/section11/main.cpp
+++ code/section11/main.cpp
@@ -13,7 +13,17 @@ int main()
 {
     Matrix3d A;
     Vector3d b;
-  
-    //TODO
+
+    A << -12, -10.8, -13.4, -18.6, -12.1, -19.6, -15.8, -10.4, -11.5;
+    b << -0.4, -0.6, -1.4;
+
+    const FullPivLU<Matrix3d>  LU(A);
+    const auto x = LU.solve(b);
+    cout << "x = [" << x << " ]\n";
+
+    const auto solution_error = (A * x - b).cwiseAbs().maxCoeff();
+
+    cout << "Rank of A: " << LU.rank() << endl;
+    cout << "Solution Error: " << solution_error << endl;
 }
 
