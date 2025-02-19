6c55f8095bbc4718dd79a075303efcaedfc28864
Mihai-Cristian Olteanu <mihaicristian.olteanu98@gmail.com>
2025-02-19 22:43:36 +0000

    Section 2.1 done

--- code/include/compute_areas_normals.h
+++ code/include/compute_areas_normals.h
@@ -10,8 +10,25 @@ void compute_areas_normals(const Eigen::MatrixXd& V,
   using namespace Eigen;
   areas.resize(F.rows());
   normals.resize(F.rows(),3);
-  
-  //TODO
+
+  for (int y = 0; y < F.rows(); y++) {
+    // Get indices for vertices
+    const int idx_v0 = F(y,0);
+    const int idx_v1 = F(y,1);
+    const int idx_v2 = F(y,2);
+
+    // Get vertices
+    const auto v0 = (RowVector3d)V.row(idx_v0).reshaped();
+    const auto v1 = (RowVector3d)V.row(idx_v1).reshaped();
+    const auto v2 = (RowVector3d)V.row(idx_v2).reshaped();
+
+    // Compute unnormalised norm and its magnitude
+    const auto unnormalised_norm = (v1 - v0).cross( v2 - v0);
+    const auto magnitude = unnormalised_norm.norm();
+
+    normals.row(y) = unnormalised_norm / magnitude;
+    areas(y) = 0.5 * magnitude;
+  }
 }
 
 
