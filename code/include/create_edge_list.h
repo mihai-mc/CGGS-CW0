89bd92b720dd8752b395e561117eb0641992b250
Mihai-Cristian Olteanu <mihaicristian.olteanu98@gmail.com>
2025-02-20 01:31:26 +0000

    Section 2.2 done

--- code/include/create_edge_list.h
+++ code/include/create_edge_list.h
@@ -13,7 +13,27 @@ void create_edge_list(const Eigen::MatrixXi& F,
   E.resize(0,2);
   boundEMask.resize(0);
  
-  //TODO
+  // Compute halfedges
+  H.resize(3 * F.rows(), 2);
+  for (int y = 0; y < F.rows(); y++) {
+      H.row(3 * y + 0) << F(y, 0), F(y, 1); // ij
+      H.row(3 * y + 1) << F(y, 1), F(y, 2); // jk
+      H.row(3 * y + 2) << F(y, 2), F(y, 0); // ki
+  }
+
+  // Get a sorted copy of H
+  MatrixXi H_copy = H;
+  sort_rows(H_copy);
+
+  std::vector<int> uniqueIndices, counts, inverse;
+  unique(H_copy, uniqueIndices, counts, inverse);
+
+  E.resize(uniqueIndices.size(), 2);
+  boundEMask.resize(E.rows());
+  for (int i = 0; i < uniqueIndices.size(); i++) {
+    E.row(i) = H_copy.row(uniqueIndices[i]);
+    boundEMask[i] = counts[i] == 1 ? 1 : 0;
+  }
 }
 
 
