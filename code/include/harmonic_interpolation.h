13a8ab7ca6e61ede6bce4d8335b6696cfc607c3e
Mihai-Cristian Olteanu <mihaicristian.olteanu98@gmail.com>
2025-02-20 02:26:51 +0000

    Section 3 done

--- code/include/harmonic_interpolation.h
+++ code/include/harmonic_interpolation.h
@@ -10,11 +10,39 @@ Eigen::VectorXd harmonic_interpolation(const Eigen::MatrixXd& V,
                                        const Eigen::MatrixXi& E,
                                        const Eigen::VectorXi& B,
                                        const Eigen::VectorXd& xB){
-  
-  //TODO
-  return Eigen::VectorXd::Zero(V.rows());
-  
-  
+
+  // Construct differential matrix d0
+  std::vector<Eigen::Triplet<double>> triplets(E.rows() * 2); // COO format
+  for (int ei = 0; ei < E.rows(); ei++) {
+    triplets[2 * ei] = {ei, E(ei, 0), -1.0}; // (ei, source(ei), -1)
+    triplets[2 * ei + 1] = {ei, E(ei, 1), +1.0}; // (ei, target(ei), +1)
+  }
+  Eigen::SparseMatrix<double> d0(E.rows(), V.rows());
+  d0.setFromTriplets(triplets.begin(), triplets.end());
+
+  // Compute I as the complement of B
+  Eigen::VectorXi I = set_diff(Eigen::VectorXi::LinSpaced(V.rows(), 0, V.rows() - 1), B);
+
+  // Slice sparse matrices
+  Eigen::SparseMatrix<double> d0I = slice_columns_sparse(d0, I);
+  Eigen::SparseMatrix<double> d0B = slice_columns_sparse(d0, B);
+
+  // Construct LHS and RHS of the equation
+  Eigen::VectorXd rhs = -1.0 * d0I.transpose() * d0B * xB;
+  Eigen::SparseMatrix<double> lhs = d0I.transpose() * d0I;
+
+  // Solver for xI
+  Eigen::SimplicialLDLT solver(lhs);
+  Eigen::VectorXd xI = solver.solve(rhs);
+
+  // Combine xI and xB into the final results
+  Eigen::VectorXd x(V.rows());
+  for (int i = 0; i < B.rows(); i++)
+    x(B(i)) = xB(i); // fixed points
+  for (int i = 0; i < I.rows(); i++)
+    x(I(i)) = xI(i); // interpolated points
+
+  return x;
 }
 
 
